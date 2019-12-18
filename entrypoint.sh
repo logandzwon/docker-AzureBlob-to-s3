5861a3588146abcc5ccf99e813e35e9e2764fc79
logandzwon <logandzwon@gmail.com>
2019-12-18 18:44:35 -0500

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,11 @@
 #! /bin/sh
 
 # mount the s3 bucket
-echo "AWS_ID:AWS_Key" > ~/.passwd-s3fs
-s3fs -o use_cache=/tmp/cache AWS_S3_BUCKET /mnt/s3bucket
+echo "$AWS_ID:$AWS_Key" > ~/.passwd-s3fs
+s3fs -o use_cache=/tmp/cache $AWS_S3_BUCKET /mnt/s3bucket
 
 # start the file transfer
-azcopy copy 'AZURE_URI?AZURE_SAS' /mnt/s3bucket --recursive
+azcopy copy "'$AZURE_URI?$AZURE_SAS'" /mnt/s3bucket --recursive
 
 # clean up and exit
 umount /mnt/s3bucket
