393bb244b0104c2c75a519229f9c613e8015d403
logandzwon <logandzwon@gmail.com>
2019-12-19 13:51:23 -0500

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,12 @@
 #! /bin/sh
 
 /node_modules/.bin/azure-s3 \
-    --concurrency "'$CONCURRENCY'" \
-    --azure-connection "'$AZURE_CONNECTION'" \
-    --azure-container "'AZURE_CONTAINER'" \
-    --aws-bucket "'$AWS_S3_BUCKET'" \
-    --aws-region "'$AWS_region'" \
-    --aws-access-key-id "'AWS_ID'" \
-    --aws-secret-access-key "'AWS_Key'"
+    --concurrency "$CONCURRENCY" \
+    --azure-connection "$AZURE_CONNECTION" \
+    --azure-container "AZURE_CONTAINER" \
+    --aws-bucket "$AWS_S3_BUCKET" \
+    --aws-region "$AWS_region" \
+    --aws-access-key-id "$AWS_ID" \
+    --aws-secret-access-key "$AWS_Key"
 
 exit 0
