7e81cecb92660c2ad333525a8cb26ac5ce227619
logandzwon <logandzwon@gmail.com>
2019-12-19 11:22:19 -0500

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,13 +1,12 @@
 #! /bin/sh
 
-# mount the s3 bucket
-echo "$AWS_ID:$AWS_Key" > ~/.passwd-s3fs
-chmod 600 ~/.passwd-s3fs
-s3fs -o use_cache=/tmp/cache $AWS_S3_BUCKET /mnt/s3bucket
+/root/node_modules/.bin/azure-s3 \
+    --concurrency "'$CONCURRENCY'" \
+    --azure-connection "'$AZURE_CONNECTION'" \
+    --azure-container "'AZURE_CONTAINER'" \
+    --aws-bucket "'$AWS_S3_BUCKET'" \
+    --aws-region "'$AWS_region'" \
+    --aws-access-key-id "'AWS_ID'" \
+    --aws-secret-access-key "'AWS_Key'"
 
-# start the file transfer
-azcopy copy "'$AZURE_URI?$AZURE_SAS'" /mnt/s3bucket --recursive
-
-# clean up and exit
-umount /mnt/s3bucket
 exit 0
