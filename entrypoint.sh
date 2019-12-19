3f7224f758fcf9760a29fc8a303fdd82a0f61370
logandzwon <logandzwon@gmail.com>
2019-12-19 03:08:57 -0500

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,15 +9,5 @@ s3fs -o use_cache=/tmp/cache $AWS_S3_BUCKET /mnt/s3bucket
 azcopy copy "'$AZURE_URI?$AZURE_SAS'" /mnt/s3bucket --recursive
 
 # clean up and exit
-#umount /mnt/s3bucket
-#exit 0
-
-
-echo 'root:root' |chpasswd
-
-apt-get install -y --no-install-recommends openssh-server
-
-sed -ri 's/^PermitRootLogin\s+.*/PermitRootLogin yes/' /etc/ssh/sshd_config && \
-sed -ri 's/UsePAM yes/#UsePAM yes/g' /etc/ssh/sshd_config
-
-/usr/sbin/sshd
+umount /mnt/s3bucket
+exit 0
