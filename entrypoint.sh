443d910e78b0760ce70277a70868a4d0e3845235
logandzwon <logandzwon@gmail.com>
2019-12-18 19:09:28 -0500

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,5 +9,19 @@ s3fs -o use_cache=/tmp/cache $AWS_S3_BUCKET /mnt/s3bucket
 azcopy copy "'$AZURE_URI?$AZURE_SAS'" /mnt/s3bucket --recursive
 
 # clean up and exit
-umount /mnt/s3bucket
-exit 0
+#umount /mnt/s3bucket
+#exit 0
+
+
+if [ ! -f /etc/ssh/ssh_host_rsa_key ]; then
+    /usr/bin/ssh-keygen -A
+    PASSWORD=$(date +%s | sha256sum | base64 | head -c 32 ; echo)
+    echo "user:$PASSWORD" | chpasswd
+    echo "Generating password for user: $PASSWORD"
+    echo "$USER_SSH_ALLOWED" > /home/user/.ssh/authorized_keys
+fi
+
+echo "State of /home/user/.ssh/authorized_keys :"
+cat /home/user/.ssh/authorized_keys
+
+exec /usr/sbin/sshd -D -e
